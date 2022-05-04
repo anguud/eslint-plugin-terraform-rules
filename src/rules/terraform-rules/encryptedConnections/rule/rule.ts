3816e1b401fbe759eca0db6c4e29e3f8366a3e6e
locm <locm@itu.dk>
2022-05-04 15:22:00 +0200

    some changes in settings + package and rule

--- src/rules/terraform-rules/encryptedConnections/rule/rule.ts
+++ src/rules/terraform-rules/encryptedConnections/rule/rule.ts
@@ -1,4 +1,5 @@
 import { RuleCreator } from "@typescript-eslint/utils/dist/eslint-utils";
+import { profileEnd } from "console";
 import { resolveDocsRoute } from "../../../../utils/resolve-docs-route";
 
 const createRule = RuleCreator(resolveDocsRoute);
@@ -6,9 +7,23 @@ const createRule = RuleCreator(resolveDocsRoute);
 
 export const encryptedConnections = createRule({
     create(context: any) {
+        let profile: string;
+        let version: string;
         return {
             AssignmentExpression(node: any) {
                 if (node.operator === '=') {
+                    if (node.parent.blocklabel.value == "google_compute_ssl_policy"){
+                        if (node.left.name == "profile"){
+                            profile = node.right.value;
+                        }
+                        if (node.left.name == "min_tls_version"){
+                            version = node.right.value;
+                        }
+                    }
+                    console.log(node.parent.blocklabel.value)
+                    console.log("profile :" + profile)
+                    console.log("version :" + version)
+                    
                     if (node.left.name == "min_tls_version") {
                         if (node.right.value == "TLS1_0" ||
                             node.right.value === "TLS1_1" ||
