4fe0eef6998ec1668ec41a5e2e150aeb41e53dcd
Mr. Mikkelsen <bjmi@itu.dk>
2022-05-09 00:01:57 +0200

    rule detects and fixes logs both when declared to false and not defined

--- src/rules/terraform-rules/enableLogging/rule/rule.ts
+++ src/rules/terraform-rules/enableLogging/rule/rule.ts
@@ -3,40 +3,96 @@ import { resolveDocsRoute } from "../../../../utils/resolve-docs-route";
 
 const createRule = RuleCreator(resolveDocsRoute);
 
-export const enableLog = createRule({
-    create (context: any) {
-        return {
-            AssignmentExpression(node : any){
-                if (node.operator === '='){
-                    if (node.left.property){
-                        if (node.left.property.name === "enable") { 
-                            if (node.parent?.nodeName === "log_config"){
-                                if(node.parent?.parent?.nodeName === "google_compute_region_backend_service"){
-                                    if(!node.nodeValue)
-                                    context.report(node.DOCUMENT_POSITION_IMPLEMENTATION_SPECIFIC, "logging is not enabled ")
-                                }
+export enum MessageIds {
+    LOG_FOUND = "log-found",
+    LOG_FIX = "log-fix",
+    NO_LOG_FOUND = "no-log-found",
+    NO_LOG_FIX = "no-log-fix",
+}
+type MyRuleOptions = []; //NO CLUE 
 
 
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    },
+export const enableLog = createRule<MyRuleOptions,MessageIds>({
     name: 'enable_log',
     meta: {
         docs: {
-            description: 'enable logs in backend severices',
-            recommended: 'error'
+            description: "Logging should be enabled for resource: google_compute_region_backend_service {{ blocklable2 }}",
+            recommended: 'error',
+            suggestion: true,
         },
         messages: {
-            error: 'enable logs in backend severices'
+            [MessageIds.LOG_FOUND]: 'logs should not be declared to false in GCP backend services',
+            [MessageIds.LOG_FIX]: "Change boolean value {{ left }} of {{ right }} to true",
+            [MessageIds.NO_LOG_FOUND]: "Logs should be declared and enabled in GCP backend severices",
+            [MessageIds.NO_LOG_FIX]: "Insert log_config block",
         },
         type: 'problem',
         fixable: 'code',
         hasSuggestions: true,
-        schema: [], 
+        schema: [],
     },
-    defaultOptions: [],
+    defaultOptions: [], // WHAT IS THIS LOUISE
+    create(context: any) {
+        return {
+            ResourceBlockStatement(node: any) {
+                if (node.blocklabel.value === "google_compute_region_backend_service") {
+                    var hasLogConfig: boolean = false
+                    node.body.forEach((argument: any) => {
+                        if (argument.type === "TFBlock") {
+                            console.log("TFBlock")
+                            if (argument.name === "log_config") {
+                                hasLogConfig = true
+                                argument.body.forEach((BlockNode: any) => {
+                                    if (BlockNode.type === "AssignmentExpression") {
+                                        if (BlockNode.left.type === "Identifier") {
+                                            if (BlockNode.left.name === "enable") {
+                                                if (BlockNode.right.type === "Identifier") {
+                                                    if (BlockNode.right.name !== "true") {
+                                                        context.report({
+                                                            node: BlockNode,
+                                                            messageId: MessageIds.LOG_FOUND,
+                                                            suggest: [
+                                                                {
+                                                                    messageId: MessageIds.LOG_FIX,
+                                                                    data: {
+                                                                        left: BlockNode.right.name,
+                                                                        right: BlockNode.left.name
+                                                                    },
+                                                                    fix: function (fixer: { replaceText: (arg0: any, arg1: string) => any; }) {
+                                                                        return fixer.replaceText(BlockNode.right, "true");
+                                                                    },
+                                                                },
+                                                            ],
+                                                        })
+                                                    }
+
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                                )
+
+                            }
+                        } 
+                    });
+                    if (!hasLogConfig){
+                        context.report({
+                            node: node,
+                            messageId: MessageIds.NO_LOG_FOUND,
+                            suggest: [
+                                {
+                                    messageId: MessageIds.NO_LOG_FIX,
+                                    fix: function (fixer: { insertTextAfter: (arg0: any, arg1: string) => any; }) {
+                                        let enableLogging = `\n\n  log_config {\n    enable = true\n  }`
+                                        return fixer.insertTextAfter(node.body[node.body.length-1], enableLogging);
+                                    },
+                                },
+                            ],
+                        })
+                    }
+                }
+            },
+        }
+    },
 });
