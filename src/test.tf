e88a923982aae89d3e5490f1bea1d9e41b7757c2
locm <locm@itu.dk>
2022-05-10 23:19:27 +0200

    clean

--- src/test.tf
+++
@@ -1,6 +0,0 @@
-resource "google_compute_ssl_policy" "vulnerable_example" { 
-              helloWorld = "something"
-              name = "production-ssl-policy"
-              min_tls_version = "TLS_1_0"
-}
-
