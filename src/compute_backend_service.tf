0e5ba061565b62622584e28d788409f03f0258fc
locm <locm@itu.dk>
2022-05-10 22:13:59 +0200

    compute_backend_service terraform

---
+++ src/compute_backend_service.tf
@@ -0,0 +1,41 @@
+
+resource "google_compute_backend_service" "scenario1 - noncompliant" {
+  name                            = "backend-production"
+  connection_draining_timeout_sec = 10
+  session_affinity                = "CLIENT_IP"
+  load_balancing_scheme           = "EXTERNAL"
+  protocol                        = "HTTP"
+
+  log_config {
+    enable = false
+  }
+}
+
+resource "google_compute_backend_service" "scenario2 - noncompliant" {
+  name                            = "backend-production"
+  connection_draining_timeout_sec = 10
+  session_affinity                = "CLIENT_IP"
+  load_balancing_scheme           = "EXTERNAL"
+  //Default value is HTTP
+  //"Ensure That App Engine Applications Enforce HTTPS Connections"
+}
+
+resource "google_compute_backend_service" "scenario3 - noncompliant" {
+  name                            = "backend-production"
+  connection_draining_timeout_sec = 10
+  session_affinity                = "CLIENT_IP"
+  load_balancing_scheme           = "EXTERNAL"
+  protocol                        = "HTTPS"
+}
+
+resource "google_compute_backend_service" "scenario3 - compliant" {
+  name                            = "backend-production"
+  connection_draining_timeout_sec = 10
+  session_affinity                = "CLIENT_IP"
+  load_balancing_scheme           = "EXTERNAL"
+  protocol                        = "HTTPS"
+
+  log_config {
+    enable = true
+  }
+}
