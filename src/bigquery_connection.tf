5cb1a4cc88057adbbbc13081b7a291f7854a242c
locm <locm@itu.dk>
2022-05-10 23:17:48 +0200

    sql_user

--- src/bigquery_connection.tf
+++ src/bigquery_connection.tf
@@ -1,14 +1,42 @@
 resource "google_bigquery_connection" "scenario1" {
     provider      = google-beta
-    friendly_name = "bigquery-connection"
+    name = "bigquery-connection"
     description   = "a riveting description"
+    cloud_sql {
+        instance_id = "google_sql_database_instance.instance.connection_name"
+        database    = "google_sql_database.db.name"
+        type        = "MS SQL"
+        credential {
+          username = "name@email.com"
+          password = "1234"
+        }
+    }
+}
+
+resource "google_bigquery_connection" "scenario2" {
+    provider      = google-beta
+    name = "bigquery-connection"
+    cloud_sql {
+        instance_id = "google_sql_database_instance.instance.connection_name"
+        database    = "google_sql_database.db.name"
+        type        = "MySQL"
+        credential {
+          username = "name@email.com"
+          password = "change-me-later"
+        }
+    }
+}
+
+resource "google_bigquery_connection" "scenario3 - " {
+    provider      = google-beta
+    name = "bigquery-connection"
     cloud_sql {
         instance_id = "google_sql_database_instance.instance.connection_name"
         database    = "google_sql_database.db.name"
         type        = "POSTGRES"
         credential {
           username = "name@email.com"
-          password = "change-me-later"
+          password = "password"
         }
     }
 }
